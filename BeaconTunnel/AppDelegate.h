b7aa20e36747bc5a6fb5d46d06f2c2573a6ba6cf
cheneyang <cheneyang@tencent.com>
2019-10-25 00:50:16 +0800

    Initial Commit

---
+++ BeaconTunnel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeaconTunnel
+//
+//  Created by cheneyang on 2019/10/25.
+//  Copyright © 2019 cheneyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BeaconTunnel/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  BeaconTunnel
+//
+//  Created by cheneyang on 2019/10/25.
+//  Copyright © 2019 cheneyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
